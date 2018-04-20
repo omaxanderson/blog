a9e37d44767dfda6430a6d333223b3f9ad6d235f
O Max Anderson <anderso2@miamioh.edu>
2018-04-19 22:01:57 -0400

    got the post page basic function working. TODO - fix the CSS conflicts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,15 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class='col-sm-6 offset-sm-3'>
   @php
     foreach ($posts as $post) {
-      echo $post->title;
+      echo "<div class='card'>";
+      echo "<div class='card-header'>";
+        echo $post->title;
+      echo "</div>";
+
+      echo "<div class='card-body'>";
+      echo "<p>";
+      echo App\User::find($post->user_id)->get()[0]->username;
+      echo "</p><p>";
       echo $post->abstract;
-      echo $post->content;
-      echo $post->user()->username;
-      //echo $post->user->username;
+      echo "</p></div></div>";
     }
 
   @endphp
+</div>
 @endsection
